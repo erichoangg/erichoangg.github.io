5b2ea5fc61749299bdc449dc8847459040c21f77
Eric Hoang <eric.hoang@colorado.edu>
2023-12-04 20:56:02 -0700

    js-final

--- js-final/js.js
+++ js-final/js.js
@@ -1,88 +1,96 @@
-// console.log("Test");
-let resultString = "";
-let phoneNum = "";
-let operatorAr = ['+', '-', 'x', '/'];
-let resultValue = 0;
-let operator = "";
-let operatorNum = "";
+let inputNum1 = document.getElementById("num1");
+let inputNum2 = document.getElementById("num2");
+const operator = document.querySelector("select");
+const calculate = document.getElementById("calculate");
+const display = document.getElementById("display");
+const error = document.getElementById("error");
+const reset = document.getElementById("reset")
 
-const buttons = document.querySelectorAll("button");
-const result = document.getElementById("result");
-const displayNumber = document.getElementById("phone-number");
+let usedNum1 = null;
+let usedNum2 = null;
+let usedOperator = null;
 
-const displayResults = (number1, op, number2) => {
-    result.innerHTML = `${number1} ${op} ${number2}`;
+
+calculate.addEventListener("click", calculateNum);
+reset.addEventListener("click", resetNum)
+
+// Function that calculates the user's input and displays it to the phone number
+function calculateNum(){
+    let result = 0;
+    let count = 0;
+
+    const num1 = parseFloat(inputNum1.value);
+    const num2 = parseFloat(inputNum2.value);
+
+    // Validating if user inputs the same number for both inputs
+    if(num1 === num2){
+        error.textContent = "Cannot use input same number in a single operation.";
+        setTimeout(() => {
+            error.textContent = "";
+            }, 2000);
+        return;
+    }
+
+    // Validating that numbers and operators aren't used twice in a row
+    if(num1 === usedNum1 || num2 === usedNum2 || num1 === usedNum2 || num2 === usedNum1 ||operator.value === usedOperator){
+        error.textContent = "Cannot use same number and/or operator twice in a row.";
+        setTimeout(() => {
+            error.textContent = "";
+          }, 2000);          
+        return;
+    }
+    // Switch statement to perform calculations
+    switch (operator.value){
+        case '+': 
+            result = num1 + num2;
+            break;
+
+        case '-':
+            result = num1 - num2;
+            break;
+
+        case '*':
+            result = num1 * num2;
+            break;
+
+        case '/':
+            result = num1 / num2;
+            break;           
+    }
+
+    let resultString = result.toString();
+
+    // For loop to count digits of result
+    for (let i = 0; i < resultString.length; i++){
+        count++;
+    }
+
+    // Validating that the digit is a whole, non-negative number
+    if (result < 0 || result % 1 != 0){
+        error.textContent = "Invalid phone number digit";
+        setTimeout(() => {
+            error.textContent = "";
+            }, 2000); 
+          return;
+    }
+
+    // Validating the the user doesn't try to cheat by appending more than 1 digit at a time
+    if (count !== 1){
+        error.textContent = "Can only add one digit to phone number at a time.";
+        setTimeout(() => {
+            error.textContent = "";
+            }, 2000); 
+        return;
+    }
+  
+    // Update used number variables so numbers and operators cannot be used twice in a row and add to phone number
+    usedNum1 = num1;
+    usedNum2 = num2;
+    usedOperator = operator.value;
+    display.textContent += result;
 }
 
-const calculation = () => {
-
-    if (operatorNum && operator && !isNaN(parseFloat(operatorNum)) && !isNaN(parseFloat(phoneNum))){
-        const num = parseFloat(operatorNum);
-        const num2 = parseFloat(phoneNum);
-        switch (operator) {
-            case '+':
-                resultValue = num + num2;
-                break;
-
-            case '-':
-                resultValue = num - num2;
-                break;
-
-            case 'x':
-                resultValue = num * num2;
-                break;
-
-            case '/':
-                resultValue = num / num2;
-                break;
-        }
-    }
-
-    displayResults(operatorNum, operator, phoneNum);
-    if (isNaN(resultValue)){
-        phoneNum = "";
-    }
-    else{
-        phoneNum = resultValue.toString();
-    }
-    result.innerHTML = resultValue;
-    displayNumber.textContent = phoneNum;
-    operator = "";
-    operatorNum = "";
+// Function that lets user start over and input new numbers
+function resetNum(){
+    display.textContent = "";
 }
-
-buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-    const value = button.dataset.value;
-
-    if(!isNaN(parseFloat(value))){
-        phoneNum += value;
-    }
-    if (value === "AC"){
-        resultString = "";
-        phoneNum = "";
-        resultValue = 0;
-    }
-
-    else if(value === "RESET"){
-        phoneNum = "";
-    }
-
-    else if(value === "="){
-    calculation();
-    phoneNum += value;
-    }
-    
-    else if(operatorAr.includes(value)){
-        operator = value;
-        operatorNum = phoneNum;
-        phoneNum = "";
-    }
-
-    if (value != "="){
-        displayResults(operatorNum, operator, phoneNum);
-    }
-
-    });
-});
-
